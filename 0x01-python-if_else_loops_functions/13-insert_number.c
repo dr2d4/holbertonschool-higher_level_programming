8bb9c67db51377a9a219cb8ea85d0ad5e767a065
Daniel E Rodriguez <dr2d4@live.com>
2019-09-11 00:15:45 -0500

    test 0

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,30 +9,36 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *add_node = malloc(sizeof(listint_t));
-	listint_t *tmp_node = *head;
-	add_node->next = NULL;
-	add_node->n = number;
+	listint_t *add_node = NULL;
+	listint_t *tmp = *head;
 
-	if (!head || !add_node)
+	if (!head)
 		return (NULL);
+
+	add_node = malloc(sizeof(listint_t));
+
+	if (!add_node)
+		return (NULL);
+
+	add_node->n = number;
+	add_node->next = NULL;
 
 	if (!*head)
 	{
 		*head = add_node;
-		return (*head);
+		return (add_node);
 	}
 
-	while (tmp_node->next)
+	while (tmp->next)
 	{
-		if (add_node->n >= tmp_node->n && add_node->n <= tmp_node->next->n)
+		if (add_node->n >= tmp->n && add_node->n <= tmp->next->n)
 		{
-			add_node->next = tmp_node->next;
-			tmp_node->next = add_node;
+			add_node->next = tmp->next;
+			tmp->next = add_node;
 			return (add_node);
 		}
 
-		tmp_node = tmp_node->next;
+		tmp = tmp->next;
 	}
 
 	return (add_node);
