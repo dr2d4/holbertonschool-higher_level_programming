b06c525b4089c34f3cd00342e94b575282d814eb
Daniel E Rodriguez <dr2d4@live.com>
2019-09-10 22:47:15 -0500

    replace tabs by spaces

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ digit = number % (-10 if number < 0 else 10)
 print('Last digit of {:d} is {:d} '.format(number, digit), end='')
 
 if digit < 6 and digit:
-	print('and is less than 6 and not 0')
+    print('and is less than 6 and not 0')
 elif digit > 5:
-	print('and is greater than 5')
+    print('and is greater than 5')
 else:
-	print('and is 0')
+    print('and is 0')
