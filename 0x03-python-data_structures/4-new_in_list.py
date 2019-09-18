e131557cdf4c34ba7154856ef89bb8e10777585b
Daniel E Rodriguez <dr2d4@live.com>
2019-09-17 21:31:10 -0500

    update permissions to files *.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 
 def new_in_list(my_list, idx, element):
-    if id >= 0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         new_list = my_list.copy()
         new_list[idx] = element
         return new_list
