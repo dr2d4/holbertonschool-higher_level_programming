0e44ac104ffc0c873d238879b0349943f5cfa8a7
Daniel E Rodriguez <dr2d4@live.com>
2019-09-17 21:27:43 -0500

    fix error return new_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,9 @@
 
 
 def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+
     if id >= 0 and idx < len(my_list):
-        new_list = my_list.copy()
         new_list[idx] = element
         return new_list
     else:
