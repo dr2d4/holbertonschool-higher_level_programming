cd688c913fc3558430df0be36bd2639b3f486113
Daniel E Rodriguez <dr2d4@live.com>
2019-09-17 23:10:52 -0500

    test 0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 
 
 def max_integer(my_list=[]):
-    if (len(my_list) >= 0 and my_list is not None):
+    if my_list is not None and len(my_list) > 0:
         max_num = 0
 
         for num in my_list:
