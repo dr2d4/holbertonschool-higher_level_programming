3fb3004731930b9bf0b7eb3fd12477019f315112
Daniel E Rodriguez <dr2d4@live.com>
2019-09-19 23:05:30 -0500

    test 0

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,9 @@
 
 
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or len(roman_string) == 0:
+        return 0
+
     rd = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     ls = len(roman_string) // 2 + len(roman_string) % 2
     rs = roman_string
