a76c2b1079af1da27c5600351f448d0fba2efe71
Daniel E Rodriguez <dr2d4@live.com>
2019-09-19 23:14:11 -0500

    return average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    div = add = 0
+
+    for item in my_list:
+        add += item[0] * item[1]
+        div += item[1]
+
+    return add / div
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -13,3 +13,4 @@
 10. Best score
 11. Multiply by using map
 12. Roman to Integer
+13. Weighted average!
