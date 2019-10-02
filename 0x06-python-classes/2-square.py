622f39a595000feb4f19566911e34d676ee59621
Daniel E Rodriguez <dr2d4@live.com>
2019-10-01 21:11:31 -0500

    class Square that defines a square by: (based on 3-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
         self.__size = size
 
         if not isinstance(self.__size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
 
         if self.__size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,6 +1,7 @@
 # 0x06-python-classes
 
 0. My first square [mandatory]
-1. Square with size
-2. Size validation
-3. Area of a square
+1. Square with size [mandatory]
+2. Size validation [mandatory]
+3. Area of a square [mandatory]
+4. Access and update private attribute mandatory [mandatory]
