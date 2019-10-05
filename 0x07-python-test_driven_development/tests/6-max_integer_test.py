e3c0661ce9dd0a5fcc3f60e911f6eb5fdce09f88
Daniel E Rodriguez <dr2d4@live.com>
2019-10-04 22:09:28 -0500

    creating “Interactive tests"

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import unittest
+
+"""
+Unittest for max_integer
+"""
+
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max(self):
+        """
+        test max number in a list
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_empty_list(self):
+        """
+        test empty list pass to max_integer
+        """
+        self.assertEqual(max_integer([]), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
