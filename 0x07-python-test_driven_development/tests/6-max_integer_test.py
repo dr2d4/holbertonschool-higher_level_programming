9f92e0d9b348d6479b19c17d1a35722154d94531
Daniel E Rodriguez <dr2d4@live.com>
2019-10-04 22:14:32 -0500

    add docstring in class

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,6 +9,9 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """
+    class TestMaxInteger
+    """
     def test_max(self):
         """
         test max number in a list
