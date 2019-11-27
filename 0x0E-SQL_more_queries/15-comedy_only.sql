9770f8b7a9b961fbba572270a39709ccd83596be
Daniel E Rodríguez <dr2d4@live.com>
2019-11-26 22:39:01 -0500

    15. Only Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- More select with join
+SELECT tv_shows.title
+FROM tv_genres
+         INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+         INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE name = 'Comedy'
+ORDER BY tv_shows.title;
