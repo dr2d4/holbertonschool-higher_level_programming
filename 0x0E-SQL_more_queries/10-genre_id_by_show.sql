6fe8a031e2d547b4ee635baf3fe51f656c0fa4ef
Daniel E Rodríguez <dr2d4@live.com>
2019-11-26 20:53:22 -0500

    10. Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- More select with join
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+         INNER JOIN tv_shows on tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
