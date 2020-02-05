7e119bb750736248a8725592cd7f07dde1bc1bc4
Daniel E Rodriguez <dr2d4@live.com>
2020-02-04 19:54:59 -0500

    Task 11 test 1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 
 const a = process.argv.slice(2);
-console.log(a.sort().reverse()[0]);
+
+if (!a.length || a.length === 1) {
+  console.log('0');
+} else {
+  console.log(a.sort().reverse()[0]);
+}
