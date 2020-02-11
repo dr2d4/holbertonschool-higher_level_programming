d70ca358b1b3048e0e51a2274fbc8cf815fd4988
Daniel E Rodriguez <dr2d4@live.com>
2020-02-10 22:27:27 -0500

    Task 3 test

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Define class Rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
